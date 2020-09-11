064d132f12c4a6602fbe1675a17d50e97ccb1d8b
Agrim Bansal <agrib@users.noreply.github.com>
2020-09-11 15:30:03 +0530

    created profile-service repo

---
+++ profile-service/src/main/java/com/agbansal/profile/ProfileApplication.java
@@ -0,0 +1,10 @@
+package com.agbansal.profile;
+
+import  org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileApplication {
+    public static void main(String args[]){
+
+    }
+}
