fb32476f6cafd3f81e7e20a33659e175163b86f9
Agrim Bansal <agrib@users.noreply.github.com>
2020-09-20 17:24:16 +0530

    Done service-registry, gateway-service

--- service-registry/src/main/java/com/agbansal/registry/RegistryApplication.java
+++ service-registry/src/main/java/com/agbansal/registry/RegistryApplication.java
@@ -1,10 +1,11 @@
 package com.agbansal.registry;
 
-import  org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RegistryApplication {
     public static void main(String args[]){
-
+        SpringApplication.run(RegistryApplication.class, args);
     }
 }
